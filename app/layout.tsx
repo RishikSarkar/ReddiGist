1f3b0f1c5e14f82cfd67df47eaae10a6d18adbf0
Rishik Sarkar <rishiksarkar123@gmail.com>
2024-10-26 22:11:52 -0400

    updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { IBM_Plex_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'ReddiGist',
@@ -25,7 +29,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="data:," />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={ibmPlexSans.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"IBM Plex Sans"',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ],
+      },
     },
   },
   plugins: [],
